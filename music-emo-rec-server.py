b30812f370efb195ed32655f8939b06c35c06055
GAOGUOLIANG <1251520647@qq.com>
2019-04-18 17:56:57 -0600

    third

--- music-emo-rec-server.py
+++ music-emo-rec-server.py
@@ -5,24 +5,32 @@ import torch.nn.functional as F
 import csv
 import time
 import _thread
+import datetime
 from torch.utils.data import Dataset, DataLoader
-# from torchvision import transforms
-# from torchvision.utils import save_image
+
+
+start_datetime = datetime.datetime.now()
+start_time = time.time()
+start_clock = time.clock()
+with open('record', 'a') as f:
+    f.write('start datetime: {}\nstart time: {}\n\
+        start clock: {}\n'.format(start_datetime, start_time, start_clock))
 
 
 # train_rate = 0.8
 train_slice_num = 2223  # 用来训练的曲子数
 pic_len = 256           # 图片长度
-batch_size = 1         
-epoch_num = 1
+batch_size = 50         
+epoch_num = 10
 # input_num = 2
-interval = 1            # 窗口间隔
-part_data_num = 100     # 每一次训读入的数据
+interval = 500            # 窗口间隔
+part_data_num = 1000     # 每一次训读入的数据
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 device = torch.device('cpu')
 
-print('start')
-
+# print('start')
+with open('record', 'a') as f:
+    f.write('start\n')
 
 
 # print('slice_num is {}'.format(len(input_slice)))
@@ -31,7 +39,7 @@ print('start')
 #     f.write('slice_num is {}\nmusic input end, start to label\n'.format(slice_num))
 
 # 读入label
-label_file = open('../data/cal500/prodLabels.csv', 'r')
+label_file = open('prodLabels.csv', 'r')
 label_reader = csv.reader(label_file)
 #print(list(label_reader)[0:5])
 reader_list = list(label_reader)
@@ -45,7 +53,6 @@ for line in reader_list:
     else:
         labels.append(list(map(int, line)))
 label_len = len(labels[0])
-label_file.close()
 #labels = list(map(int, label_reader))[0:20]
 # print(labels[input_num-1])
 
@@ -82,7 +89,7 @@ def transfer_data():
     global test_data_has_refreshed
 
     # 读入音频数据，计算数据行数
-    input_file = open('../data/cal500/prodAudios_v2.txt', 'r')
+    input_file = open('prodAudios_v2.txt', 'r')
     input_lines = input_file.readlines()
     # create_inp_list = True
     slice_num = 0
@@ -92,7 +99,8 @@ def transfer_data():
         line.pop()
         line = list(map(int, line))
         line_len = len(line)
-        print(line_len)
+        with open('record', 'a') as f:
+            f.write(str(line_len) + '\n')
         # print(line_len)
         sample_start = 0
         sample_len = pic_len*pic_len
@@ -146,22 +154,22 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(2, 6, 5)
         self.norm1 = nn.BatchNorm2d(6)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.norm2 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(6, 6, 7)
-        self.conv4 = nn.Conv2d(6, 6, 5)
-        linear_len = int(((((pic_len - 4) / 2 - 4) / 2 - 6) / 2 - 4) / 2)
+        self.conv3 = nn.Conv2d(16, 16, 7)
+        self.conv4 = nn.Conv2d(16, 8, 5)
+        linear_len = int(((pic_len - 4) / 2 - 4) / 2 - 6 - 4)
         self.linear_len = linear_len
-        self.fc1 = nn.Linear(6 * linear_len * linear_len, 500)
-        self.fc2 = nn.Linear(500, 100)
-        self.fc3 = nn.Linear(100, 20)
-        self.fc4 = nn.Linear(20, 18)
+        self.fc1 = nn.Linear(8 * linear_len * linear_len, 5000)
+        self.fc2 = nn.Linear(5000, 1000)
+        self.fc3 = nn.Linear(1000, 200)
+        self.fc4 = nn.Linear(200, 18)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = (F.relu(self.conv3(x)))
+        x = (F.relu(self.conv4(x)))
         x = x.view(-1, 6 * self.linear_len * self.linear_len)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -216,23 +224,25 @@ def train():
 
                 # forward + backward + optimize
                 outputs = net(inputs)
-                outputs = torch.round(outputs)
+                # outputs = torch.round(outputs)
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
 
                 # print statistics
                 running_loss += loss.item()
-                if i % 100 == 99:    # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 500))
-                    # with open('record', 'a') as f:
-                    #     f.write('[%d, %5d] loss: %.3f\n\n' %
-                    #         (epoch + 1, i + 1, running_loss / 2000))
+                if i % 500 == 499:    # print every 2000 mini-batches
+                    # print('[%d, %5d] loss: %.3f' %
+                    #       (epoch + 1, i + 1, running_loss / 500))
+                    with open('record', 'a') as f:
+                        f.write('[%d, %5d] loss: %.3f\n\n' %
+                            (epoch + 1, i + 1, running_loss / 500))
                     running_loss = 0.0
 
 
-    print('Finished Training')
+    # print('Finished Training')
+    with open('record', 'a') as f:
+        f.write('Finished Training\n')
 
 
 def test():
@@ -286,6 +296,14 @@ def test():
     print('Loss of the network: {}'.format(loss))
     print('My_Accuracy of the network on the test images: %d %%' % (
         100 * correct_v2 / total))
+    with open('record', 'a') as f:
+        f.write('Accuracy of the network on the test images: %d %%\n' % (
+            100 * correct / total))
+    with open('record', 'a') as f:
+        f.write('Loss of the network: {}\n'.format(loss))
+    with open('record', 'a') as f:
+        f.write('My_Accuracy of the network on the test images: %d %%\n' % (
+            100 * correct_v2 / total))
 
 
 
@@ -293,5 +311,14 @@ if __name__ == "__main__":
     _thread.start_new_thread(transfer_data, ())
     train()
     test()
+    end_datetime = datetime.datetime.now()
+    end_time = time.time()
+    end_clock = time.clock()
+    with open('record', 'a') as f:
+        f.write('end datetime: {}\nend time: {}\nend clock: {}\n\
+            dur_datetime: {}\ndur_time: {}\ndur_clock: {}\n\n\n\n'.
+            format(end_datetime, end_time, end_clock, 
+            (end_datetime - start_datetime).seconds,
+            end_time - start_time, end_clock - start_clock))
 
 
